41125387aa8e940ec0b11c4c9100fd9517b465d8
18690556 <AMNagovitsyn@sberbank.ru>
2021-07-07 17:46:43 +0300

    BE

--- src/main/java/io/polybius/phonevalidator/MobilePhoneNumberValidator.java
+++ src/main/java/io/polybius/phonevalidator/MobilePhoneNumberValidator.java
@@ -5,55 +5,61 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MobilePhoneNumberValidator {
-  public ValidationResultDto validate(List<String> phoneNumbers) {
-    ValidationResultDto result = new ValidationResultDto();
-    result.invalidPhones = new ArrayList<>();
-    result.validPhonesByCountry = new HashMap<>();
-    for (int i = 0; i < phoneNumbers.size(); i++) {
-      String phoneNumber = phoneNumbers.get(i);
-      boolean isValid;
-      String country = null;
-      if(phoneNumber.startsWith("370")||phoneNumber.startsWith("+370")) {
-        country = "LT";
-        phoneNumber = phoneNumber.replaceAll("\\)", "").replaceAll("\\(", "").replaceAll(" ", "").replaceAll("-", "");
-        if (phoneNumber.startsWith("370")) {
-          isValid = phoneNumber.charAt(3) == '6' && phoneNumber.substring(3).length() == 8;
-        }else{
-          isValid = phoneNumber.charAt(4) == '6' && phoneNumber.substring(4).length() == 8;
-        }
-      } else if (phoneNumber.startsWith("+371") || phoneNumber.startsWith("371")) {
-        country = "LV";
-        if (phoneNumber.startsWith("370")) {
-          isValid = phoneNumber.charAt(3) == '2' && phoneNumber.substring(3).length() == 8;
-        }
-        else {
-          isValid = phoneNumber.charAt(4) == '2' && phoneNumber.substring(4).length() == 8;
-        }
-      } else if (phoneNumber.startsWith("372")) {
-        country = "EE";
-        phoneNumber = phoneNumber.replaceAll("\\)", "").replaceAll("\\(", "").replaceAll(" ", "").replaceAll("-", "");
-        if (phoneNumber.startsWith("+372")) {
-          isValid = phoneNumber.charAt(4) == '5' && (phoneNumber.substring(4).length() == 7
-              || phoneNumber.substring(4).length() == 8);
-        } else {
-          isValid = phoneNumber.charAt(3) == '5' && phoneNumber.substring(3).length() == 7;
-        }
-      } else {
-        isValid = false;
-      }
+    public ValidationResultDto validate(List<String> phoneNumbers) {
+        ValidationResultDto result = new ValidationResultDto();
+        result.invalidPhones = new ArrayList<>();
+        result.validPhonesByCountry = new HashMap<>();
+        for (int i = 0; i < phoneNumbers.size(); i++) {
+            String phoneNumber = phoneNumbers.get(i);
+            boolean isValid;
+            String country = null;
+            long leftBraceCount = phoneNumber.chars().filter(ch -> ch == '(').count();
+            long rightBraceCount = phoneNumber.chars().filter(ch -> ch == ')').count();
+            String formatNumber = phoneNumber;
+            isValid = leftBraceCount == rightBraceCount;
+            if (isValid) {
+                formatNumber = phoneNumber
+                        .replaceAll("\\s", "")
+                        .replaceAll("\\(", "")
+                        .replaceAll("\\)", "")
+                        .replaceAll("-", "")
+                        .replaceFirst("^\\+", "");
+            }
+            if (isValid) {
+                if (formatNumber.startsWith("370")) {
+                    country = "LT";
+                    formatNumber = formatNumber.replaceFirst("^(370)", "");
+                    isValid = formatNumber.startsWith("6") && formatNumber.length() == 8;
+                } else if (formatNumber.startsWith("371")) {
+                    country = "LV";
+                    formatNumber = formatNumber.replaceFirst("^(371)", "");
+                    isValid = formatNumber.startsWith("2") && formatNumber.length() == 8;
+                } else if (formatNumber.startsWith("372")) {
+                    country = "EE";
+                    formatNumber = formatNumber.replaceFirst("^(372)", "");
+                    isValid = formatNumber.startsWith("5") && (formatNumber.length() == 7 || formatNumber.length() == 8);
+                } else if (formatNumber.startsWith("32")) {
+                    country = "BE";
+                    formatNumber = formatNumber.replaceFirst("^(32)", "");
+                    isValid = formatNumber.length() == 9 && (formatNumber.startsWith("456") ||
+                            formatNumber.startsWith("47") ||
+                            formatNumber.startsWith("48") ||
+                            formatNumber.startsWith("49"));
+                } else {
+                    isValid = false;
+                }
+            }
 
-      if (isValid) {
-        if (!result.validPhonesByCountry.containsKey(country)) {
-          result.validPhonesByCountry.put(country, new ArrayList<>());
-        }
+            if (isValid) {
+                if (!result.validPhonesByCountry.containsKey(country)) {
+                    result.validPhonesByCountry.put(country, new ArrayList<>());
+                }
 
-        result.validPhonesByCountry.get(country).add(phoneNumbers.get(0));
-      } else {
-        result.invalidPhones.add(phoneNumbers.get(i));
-      }
+                result.validPhonesByCountry.get(country).add(phoneNumbers.get(0));
+            } else {
+                result.invalidPhones.add(phoneNumbers.get(i));
+            }
+        }
+        return result;
     }
-
-
-    return result;
-  }
 }
--- src/test/java/io/polybius/phonevalidator/MobilePhoneNumberValidatorTest.java
+++ src/test/java/io/polybius/phonevalidator/MobilePhoneNumberValidatorTest.java
@@ -8,17 +8,71 @@ import static org.junit.Assert.assertEquals;
 
 public class MobilePhoneNumberValidatorTest {
 
-  private MobilePhoneNumberValidator validator = new MobilePhoneNumberValidator();
+    private final MobilePhoneNumberValidator validator = new MobilePhoneNumberValidator();
 
-  @Test
-  public void validate() {
-    ValidationResultDto result = validator.validate(List.of("+37061234567"));
-    assertEquals(List.of("+37061234567"), result.validPhonesByCountry.get("LT"));
+    @Test
+    public void validateLT() {
+        ValidationResultDto result = validator.validate(List.of("+370(6)123 45-67"));
+        assertEquals(List.of("+370(6)123 45-67"), result.validPhonesByCountry.get("LT"));
+    }
 
-    result = validator.validate(List.of("+37091234567"));
-    assertEquals(List.of("+37091234567"), result.invalidPhones);
+    @Test
+    public void invalidateLT() {
+        ValidationResultDto result = validator.validate(List.of("+37091234567"));
+        assertEquals(List.of("+37091234567"), result.invalidPhones);
 
-    result = validator.validate(List.of("+3706123456"));
-    assertEquals(List.of("+3706123456"), result.invalidPhones);
-  }
+        result = validator.validate(List.of("+3706123456"));
+        assertEquals(List.of("+3706123456"), result.invalidPhones);
+
+        result = validator.validate(List.of("+370(6123456"));
+        assertEquals(List.of("+370(6123456"), result.invalidPhones);
+
+        result = validator.validate(List.of("+370+6123+456"));
+        assertEquals(List.of("+370+6123+456"), result.invalidPhones);
+
+        result = validator.validate(List.of("370655555(("));
+        assertEquals(List.of("370655555(("), result.invalidPhones);
+    }
+
+    @Test
+    public void validateBE() {
+        ValidationResultDto result = validator.validate(List.of("32456123456"));
+        assertEquals(List.of("32456123456"), result.validPhonesByCountry.get("BE"));
+
+        result = validator.validate(List.of("+324(5)612-34 56"));
+        assertEquals(List.of("+324(5)612-34 56"), result.validPhonesByCountry.get("BE"));
+
+        result = validator.validate(List.of("+3247 12-34 5(6)7"));
+        assertEquals(List.of("+3247 12-34 5(6)7"), result.validPhonesByCountry.get("BE"));
+
+        result = validator.validate(List.of("+324(5)612-34 56"));
+        assertEquals(List.of("+324(5)612-34 56"), result.validPhonesByCountry.get("BE"));
+    }
+
+    @Test
+    public void invalidateBE() {
+        ValidationResultDto result = validator.validate(List.of("33456123456"));
+        assertEquals(List.of("33456123456"), result.invalidPhones);
+
+        result = validator.validate(List.of("+324561234560"));
+        assertEquals(List.of("+324561234560"), result.invalidPhones);
+    }
+
+    @Test
+    public void validateLV() {
+        ValidationResultDto result = validator.validate(List.of("371(2)456-12 34"));
+        assertEquals(List.of("371(2)456-12 34"), result.validPhonesByCountry.get("LV"));
+
+        result = validator.validate(List.of("+37124561234"));
+        assertEquals(List.of("+37124561234"), result.validPhonesByCountry.get("LV"));
+    }
+
+    @Test
+    public void validateEE() {
+        ValidationResultDto result = validator.validate(List.of("372-(5)-561 2345"));
+        assertEquals(List.of("372-(5)-561 2345"), result.validPhonesByCountry.get("EE"));
+
+        result = validator.validate(List.of("+372-(5)-561 2345"));
+        assertEquals(List.of("+372-(5)-561 2345"), result.validPhonesByCountry.get("EE"));
+    }
 }
